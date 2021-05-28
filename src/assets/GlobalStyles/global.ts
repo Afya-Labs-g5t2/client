861f96925819d34d8dd989cb886845ba3c79ccb9
Mark Alves <lamarques.marcos@gmail.com>
2021-05-28 11:05:26 -0300

    Create global style and assets folder

---
+++ src/assets/GlobalStyles/global.ts
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+    *{
+        padding: 0;
+        margin: 0;
+        font-family: 'Inter', 'Roboto', sans-serif;
+    }
+
+    html {
+        scroll-behavior: smooth;
+    }
+`
