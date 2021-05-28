6e4d0d38ed6db9dc36026d310ed0a562e7b9d0cb
Mark Alves <lamarques.marcos@gmail.com>
2021-05-28 16:34:06 -0300

    Remove space in User css fiel

--- src/views/pages/User/styles.ts
+++ src/views/pages/User/styles.ts
@@ -8,30 +8,21 @@ export const SectionComponent = styled.section`
         position: absolute;
         right: 0;
         top: 0;
-
         padding-top: 20%;
-
         border: 1px solid;
-
         z-index: 1;
-
         transition: transform .3s;
-
         transform: translateX(100%);
 
         .top-section {
             display: flex;
             flex-direction: row;
-
             font-weight: 800;
             font-size: 1.75rem;
-            
             padding-left: 16px;
             padding-right: 16px;
-
             align-items: center;
             justify-content: space-between;
-
             border-bottom: 1px solid #C4C4C4;
 
             .icon {
@@ -44,20 +35,15 @@ export const SectionComponent = styled.section`
             padding: 16px;
             align-items: center;
             font-weight: 400;
-
             border-bottom: 1px solid #C4C4C4;
 
             .photo-container {
                 height: 56px;
                 width: 56px;
-
                 margin-bottom: 12px;
-
                 background: #c4c4c4;
-
                 background-image: url("https://images.pexels.com/photos/3692749/pexels-photo-3692749.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260");
                 background-size: cover;
-
                 border-radius: 9999px;
             }
 
@@ -68,7 +54,6 @@ export const SectionComponent = styled.section`
 
         .action-section {
             margin: 16px;
-
             display: flex;
             flex-direction: row;
 
@@ -81,7 +66,6 @@ export const SectionComponent = styled.section`
             display: flex;
             align-items: center;
             justify-content: center;
-
             margin-top: 1.5rem;
 
             .btn {
@@ -90,7 +74,6 @@ export const SectionComponent = styled.section`
                 height: 2.125rem;
                 border-radius: 9px;
                 background: var(--red);
-
                 align-items: center;
                 justify-content: center;
                 
