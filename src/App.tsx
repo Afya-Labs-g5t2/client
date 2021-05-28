5f0037db2eec414ce4fee911492075196cd0d831
Mark Alves <lamarques.marcos@gmail.com>
2021-05-28 11:22:39 -0300

    Link global style to app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import GlobalStyles from './assets/GlobalStyles'
 import Routes from './routes';
 
 function App() {
   return (
     <>
+    <GlobalStyles/>
     <Routes/>
 
     </>
---
+++ src/views/pages/User/styles.ts
@@ -0,0 +1,5 @@
+import styled from 'styled-components'
+
+export const SectionComponent = styled.section`
+    background: #c4c4c4;
+`
