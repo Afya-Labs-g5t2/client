691cfa89afc053a79163794e4162f3385c028630
Mark Alves <lamarques.marcos@gmail.com>
2021-05-31 16:42:10 -0300

    Fix typo

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,8 +1,14 @@
-import React fomr 'react';
+import React from 'react';
+import FullCalendar from '@fullcalendar/react'
+import daygridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
 
 const Calendar: React.FC = () => {
     return (
         <div className="calendar">
+            <FullCalendar
+                plugins={[daygridPlugin]}
+            />
         </div>
     )
 }
