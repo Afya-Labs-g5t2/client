9a7cc0843e1866c05b9fbf3282a7a708ed657535
michele <michelleset@hotmail.com>
2021-05-31 19:09:32 -0300

    Fazendo o checkout main

--- src/views/pages/Home/style.ts
+++ src/views/pages/Home/style.ts
@@ -9,16 +9,16 @@ export const HomeStyle = styled.div`
   box-sizing: border-box;
 }
    
-  .container{
-margin-top: 20px;
-background:E5E5E5;
-align-items: center;
-text-align: center;
-justify-content: center;
-padding:10px;
+.container{
+    margin-top: 20px;
+    background:E5E5E5;
+    align-items: center;
+    text-align: center;
+    justify-content: center;
+    padding:10px;
 
   }
-  .nav{
+.nav{
       display: flex;
       text-align: center;
       width:100%;
@@ -26,16 +26,16 @@ padding:10px;
       justify-content: space-around;
    
   }
-  .nav div{
+.nav div{
       margin: 20px;
   }
-  .texto_home{
+.texto_home{
     font-size: 13px;
     text-align: left;
     justify-content: flex-start; 
     margin-right: 50px;
   }
-  .image{
+.image{
       width: 60px;
       height: 60px;
       border-radius: 50%;
@@ -45,53 +45,52 @@ padding:10px;
     display: block;
     width: 100%;
     margin-top: 60px;
-}
+ }
 
 .principal a button{
-font-size: 15px;
-background: #da4478;
-color: #fff;
-width: 90%;
-height: 8vh;
-font-weight: bold;
-padding:10px;
-border-radius:8%;
-margin: 10px;
-margin-top: 25px;
-border-color: white;
+
+    font-size: 15px;
+    background: #da4478;
+    color: #fff;
+    width: 90%;
+    height: 8vh;
+    font-weight: bold;
+    padding:10px;
+    border-radius:8%;
+    margin: 10px;
+    margin-top: 25px;
+    border-color: white;
 }
 
 @media screen and (min-width:400px){
   
 .container{
-
-background:E5E5E5;
-align-items: center;
-text-align: center;
-justify-content: center;
-padding:10px;
-
-  }
-  .nav{
+    background:E5E5E5;
+    align-items: center;
+    text-align: center;
+    justify-content: center;
+    padding:10px;
+}
+.nav{
     margin-top: 2rem;
   }
-  .principal{
+.principal{
     margin-bottom:1.5rem
 
   }
-  .texto_home{
+.texto_home{
     font-size: 1rem;
   }
-  .principal a button{
-font-size: 1.2rem;
-width: 75%;
-height: 10vh;
-padding:10px;
-border-radius:8%;
-margin: 10px;
-margin-top: 2rem;
+.principal a button{
+    font-size: 1.2rem;
+    width: 75%;
+    height: 10vh;
+    padding:10px;
+    border-radius:8%;
+    margin: 10px;
+    margin-top: 2rem;
 }
  
-  }
+}
   
 `
