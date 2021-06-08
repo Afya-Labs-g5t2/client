175ad9328b8e63287034f8ec57e8f8b04159fa25
Vinicius Carvalho <vini.costacarvalho@gmail.com>
2021-06-07 22:30:42 -0300

    style and layout  of CardProntuario

--- src/components/CardProntuario/index.tsx
+++ src/components/CardProntuario/index.tsx
@@ -13,14 +13,25 @@ function CardProntuario(props: ProntuarioProps) {
     <DivComponent>
       <div className="card-prontuario-container" onClick={() => props.handleClick(props.id)}>
         <div className="info-container">
-          <div className="paciente-container">
-          <span className="paciente-value">Paciente: {props.nomePaciente}</span>
-            </div>
-            <div className="especialista-container">
-              <span className="especialista-value">Especialista: {props.nomeEspecialista}</span>
+          <div className="especialista-container">
+              <p className="especialista-header">Especialista: <span className="especialista-value"> {props.nomeEspecialista}</span></p>
             </div>
             <div className="data-container">
-              <span className="data-value">Data: {props.dataProntuario}</span>
+            <p className="data-header">Data: <span className="data-value">{props.dataProntuario}</span></p>
+              <p className="data-header">Hora: <span className="data-value">00:00</span></p>
+            </div>
+            <div className="descricao-container">
+              <span className="descricao-value">Descrição:</span> 
+              <p>Lorem ipsum dolor sit amet, 
+              consectetur adipiscing elit. Mauris laoreet ex urna, a accumsan ligula iaculis sagittis. 
+              Cras sollicitudin dui sed leo eleifend, pellentesque tempor nisi pharetra. In risus nisl, 
+              rhoncus quis quam eu, egestas hendrerit nulla.
+              Sed facilisis blandit vulputate. Nullam lacinia, ante nec interdum pellentesque
+              consectetur adipiscing elit. Mauris laoreet ex urna, a accumsan ligula iaculis sagittis. 
+              Cras sollicitudin dui sed leo eleifend, pellentesque tempor nisi pharetra. In risus nisl, 
+              rhoncus quis quam eu, egestas hendrerit nulla.
+              Sed facilisis blandit vulputate. Nullam lacinia, ante nec interdum pellentesque
+              </p>
             </div>
         </div>
         <div className="icon-container">
--- src/components/CardProntuario/styles.ts
+++ src/components/CardProntuario/styles.ts
@@ -6,14 +6,16 @@ export const DivComponent = styled.div`
         flex-direction: row;
         justify-content: space-between;
         background: var(--background-main);
-        width: 22rem;
-        height: 5rem;
+        width: 21.2rem;
+        height: auto;
         border-radius: 6px;
         padding: 6px;
-        margin: .5rem 0;
+        margin: 0.5rem 0 1rem;
         box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
         user-select: none;
         cursor: pointer;
+        text-align: justify;
+        min-width: 20.5rem;
 
         .info-container {
             display: flex;
@@ -26,6 +28,46 @@ export const DivComponent = styled.div`
 
                 .paciente-value{
                     /* font-weight: 700; */
+                }
+            }
+            
+            .especialista-container{
+                padding: 1rem;
+
+                .especialista-header{
+                    font-weight: bold;
+                }
+
+                .especialista-value{
+                    font-weight: 500;
+                }
+            }
+
+            .data-container{
+                padding: 0 1rem 3rem;
+                display: flex;
+                justify-content: space-between;
+
+                .data-header{
+                font-weight: bold;
+                }
+
+                .data-value{
+                  font-weight: 500;
+                }
+            }
+            
+
+            .descricao-container{
+                padding: 0 1rem 0;
+                
+                .descricao-value{
+                    font-weight: bold;
+                    margin-bottom: 1rem;
+                }
+                p {
+                    padding: 1rem 0 1rem;
+
                 }
             }
         }
