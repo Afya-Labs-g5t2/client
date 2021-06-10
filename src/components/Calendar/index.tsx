c6adaa4958a9027aea2b152f2225e957c0b12f1b
Mark Alves <lamarques.marcos@gmail.com>
2021-06-10 11:23:45 -0300

    Create calendar component with click and local in pt

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,15 +1,63 @@
-import React, { useCallback, useRef } from 'react';
+import React, { useState } from 'react';
+import DayPicker from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
 import { DivComponent } from './styles'
 
 function Calendar() {
+
+  const [selectedDay, setSelectedDay] = useState<any>(null)
+
+  const WEEKDAYS_SHORT = {
+    pt: ['Do', 'Se', 'Te', 'Qa', 'Qi', 'Sx', 'Sa'],
+  };
+
+  const MONTHS  = {
+    pt: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+  };
+
+  const WEEKDAYS_LONG = {
+    pt: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
+  };
+
+  const FIRST_DAY_OF_WEEK = {
+    pt: 1,
+  };
+
+  const LABELS = {
+    pt: { nextMonth: 'Próximo mês', previousMonth: 'Mês anterior' },
+  };
+
+  function handleDayClick(day: {}, { selected }: any) {
+
+    selected ? setSelectedDay(undefined) : setSelectedDay(day)
     
-    return (
-        <div className="calendar-container">
-            <DivComponent>
-                <h1>Agenda</h1>
-            </DivComponent>
-        </div>
-    );
+    console.log(typeof(day))
+    console.log(typeof(selected))
   }
+    
+  return (
+      <div className="calendar-container">
+          <DivComponent>
+              <h1>Agenda</h1>
+              <DayPicker
+                months={MONTHS['pt']}
+                weekdaysLong={WEEKDAYS_LONG['pt']}
+                weekdaysShort={WEEKDAYS_SHORT['pt']}
+                firstDayOfWeek={FIRST_DAY_OF_WEEK['pt']}
+                labels={LABELS['pt']}
+                showOutsideDays
+                month={new Date()}
+                selectedDays={selectedDay}
+                onDayClick={handleDayClick}
+              />
+              <p>
+              {selectedDay
+                ? selectedDay?.toString()
+                : 'Please select a day 👻'}
+            </p>
+          </DivComponent>
+      </div>
+  );
+}
 
 export default Calendar;
