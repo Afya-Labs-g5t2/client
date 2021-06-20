5d4cd62f27a207dcee2bf7729b32af4e148d63d4
Mark Alves <lamarques.marcos@gmail.com>
2021-06-20 13:35:35 -0300

    Update logo

---
+++ src/assets/Logo.tsx
@@ -0,0 +1,99 @@
+import React from "react";
+
+function Logo() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="874"
+      height="631"
+      version="1.1"
+      viewBox="0 0 87.405 63.122"
+      className="logo-component"
+    >
+      <g fill="#fff" transform="matrix(1.68496 0 0 1.04058 -198.16 -137.463)">
+        <rect
+          width="3.969"
+          height="5.008"
+          x="121.048"
+          y="140.61"
+          fill="#fff"
+          fillOpacity="1"
+          stroke="#7D7D7D"
+          strokeDasharray="none"
+          strokeLinecap="round"
+          strokeMiterlimit="4"
+          strokeWidth="0.3"
+          className="logo-i"
+          display="inline"
+          ry="0"
+        ></rect>
+        <rect
+          width="3.969"
+          height="23.718"
+          x="121.048"
+          y="149.917"
+          fill="none"
+          fillOpacity="1"
+          stroke="#7D7D7D"
+          strokeDasharray="none"
+          strokeLinecap="round"
+          strokeMiterlimit="4"
+          strokeWidth="0.3"
+          className="logo-i"
+          display="inline"
+          ry="0"
+        ></rect>
+      </g>
+      <g
+        fill="#15191d"
+        fillOpacity="1"
+        transform="matrix(1.68496 0 0 1.6683 -174.742 -227.669)"
+      >
+        <path
+          fill="#15191d"
+          fillOpacity="1"
+          stroke="none"
+          strokeLinecap="butt"
+          strokeLinejoin="miter"
+          strokeOpacity="1"
+          strokeWidth="0.265"
+          d="M118.625 153.122l1.871-9.956h12.696v-3.808h-15.636L113.948 158h3.675s2.349-3.536 6.863-3.2c4.524.338 6.375 3.845 6.373 6.702-.003 3.542-1.977 7.161-6.688 7.256-5.751.115-8.486-4.745-8.486-4.745l-2.405 2.272s2.806 6.214 10.758 5.947c7.95-.267 10.786-5.617 10.905-9.761.12-4.237-1.652-8.626-5.546-10.103-3.848-1.46-7.493-1.323-10.772.754z"
+          className="logo-52"
+        ></path>
+      </g>
+      <g fill="#00f" transform="matrix(1.68496 0 0 1.6683 -209.246 -245.627)">
+        <path
+          fill="#fff"
+          fillOpacity="1"
+          stroke="#7D7D7D"
+          strokeLinecap="butt"
+          strokeLinejoin="miter"
+          strokeOpacity="1"
+          strokeWidth="0.265"
+          d="M158.146 172.506h15.569v3.608h-20.313s-.219-8.173 6.085-11.407c6.367-3.267 9.147-5.004 9.318-7.07.17-2.047-1.743-4.893-5.982-4.508-3.393.308-6.347 3.675-6.347 3.675l-2.807-2.873s4.15-4.57 10.758-4.277c4.531.202 6.724 2.001 8.018 4.21.97 1.657.964 4.178.269 5.761-1.023 2.329-3.361 4.19-5.748 5.264-4.009 1.804-8.419 4.075-8.82 7.617z"
+          className="logo-52"
+        ></path>
+      </g>
+      <path
+        fill="#D31C5B"
+        d="M60.802 18.195c-4.845-.431-8.135 4.197-9.302 5.536-1.167-1.339-4.457-5.967-9.302-5.536-4.135.368-6.34 4.664-4.428 8.488 1.845 3.69 6.666 5.545 12.178 10.44l1.552 1.37 1.552-1.37c5.512-4.895 10.333-6.75 12.178-10.44 1.913-3.824-.293-8.12-4.428-8.488z"
+      ></path>
+      <g fill="#fff" transform="matrix(1.15828 0 0 .79106 -84.009 -93.95)">
+        <path
+          fill="#fff"
+          fillOpacity="1"
+          stroke="#7D7D7D"
+          strokeLinecap="butt"
+          strokeLinejoin="miter"
+          strokeOpacity="1"
+          strokeWidth="0.265"
+          d="M86.764 172.074h3.118l1.89-2.976s.18-.337.52-.284c.309.049.452.345.452.345l3.091 6.79 3.685-10.253s.09-.368.502-.366c.366 0 .49.366.49.366l3.308 8.22 1.512-1.889h4.913s.52.043.507.677a.575.575 0 01-.507.599l-4.254-.04-2.005 2.305s-.174.213-.522.197c-.298-.015-.447-.33-.447-.33l-2.924-7.791-3.683 10.398s-.13.394-.464.394c-.348-.001-.512-.128-.58-.295-.066-.167-3.22-7.187-3.22-7.187l-1.65 2.538-3.732-.048c-.183-.066-.43-.2-.434-.64-.003-.352.058-.584.434-.73z"
+          className="logo-symbol"
+          display="inline"
+        ></path>
+      </g>
+    </svg>
+  );
+}
+
+export default Logo;
--- src/views/pages/Login/styles.ts
+++ src/views/pages/Login/styles.ts
@@ -3,10 +3,17 @@ import styled from 'styled-components'
 export const DivComponent = styled.div`
     .container {
         display: flex;
+        height: 100vh;
         flex-direction: column;
         align-items: center;
         justify-items: center;
-        margin-top: 10rem;
+
+        .logo-component {
+            position: absolute;
+            margin-top: 5rem;
+            width: 15rem;
+            height: auto;
+        }
      }
  @media only screen and (min-width: 700px) {
      .container {
